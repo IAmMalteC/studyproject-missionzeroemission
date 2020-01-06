6fc4154a596a9778f38e1169e1c93fe2b2d09e8e
SysRQ95 <33806385+SysRQ95@users.noreply.github.com>
2020-01-06 11:03:36 +0100

    git commit index.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,29 +1,49 @@
 
 const express = require('express');
 const app = express();
-const MariaDB = require('MariaDB');
+const mariadb = require('mariadb');
 const bodyParser = require('body-parser');
 //body Parser is a middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const connection = MariaDB.createConnection({
-  host: "141.45.92.87",
-  user: "phpmyadmin",
-  password : "Q2Jf6kY4aQuM",
-  database : "NemoDB"
-});
-//connection.getConnection();
+function getConnection() {
+  return mariadb.createConnection({
+    host: "141.45.92.87",
+    user: "root",
+    password: "r2qUh75W",
+    database: "nemodb"
+  });
+}
 
-app.post("/profil.html" , function (req , res) {
-  console.log(req.body);
-  var sql = "INSERT INTO Profil_TB(profil_name, profil_vorname, profil_firma, profil_position, profil_fax, profil_tel, profil_email, profil_benutzername, profil_passwort) VALUES (null , " + req.body.NachnameInput + "' , '"+req.body.VornameInput+"' , '"+req.body.FirmennameInput+"', '"+req.body.PositionInput+"' ,'"+req.body.FaxInput+"' , '"+req.body.TelefonInput+"' , '"+req.body.EmailInput+"' , '"+ req.body.BenutzernameInput +"' , '"+req.body.PasswortInput+"')";
-  connection.query(sql , function (err) {
-    if (err) throw err;
-    res.send("Data Is added to the database...")
-  })
+app.post('profil.html', (req , res) => {
+  console.log("First name: " + req.body.VornameInput);
+  const Vorname = req.body.VornameInput;
+  const Nachname = req.body.NachnameInput;
+
+  const queryString = "INSERT INTO NemoDB.Profil_TB VALUES (default,?,?,?,?,?,?,?,?,?)";
+  getConnection().query(queryString, [Vorname , Nachname] , (err, result , fields) => {} );
+  if (err) {
+    Console.log ("Failed to update user data..." + err);
+    res.sendStatus(500);
+    return
+  }
+  console.log("Inserted new user");
+  res.end()
 });
-app.listen("3306");
+
+app.listen(3306);
+
+//app.post("/profil.html" , function (req , res) {
+//  console.log(req.body);
+//  let sql = "INSERT INTO Profil_TB(profil_name, profil_vorname, profil_firma, profil_position, profil_fax, profil_tel, profil_email, profil_benutzername, profil_passwort) VALUES (null ,'" + req.body.NachnameInput + "' , '"+req.body.VornameInput+"' , '"+req.body.FirmennameInput+"', '"+req.body.PositionInput+"' ,'"+req.body.FaxInput+"' , '"+req.body.TelefonInput+"' , '"+req.body.EmailInput+"' , '"+ req.body.BenutzernameInput +"' , '"+req.body.PasswortInput+"')";
+//  connection.query(sql , function (err) {
+//    if (err) throw err;
+//    res.send("Data Is added to the database...")
+//  })
+//});
+//app.timeout = 0;
+//app.listen("3306");
 
 // comment being the name of the database
 
