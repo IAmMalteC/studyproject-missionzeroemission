25a3abee78b9eb352bb6f4fd57f0d508c8f3a91d
Malte <s0563114@htw-berlin.de>
2020-01-10 14:32:37 +0100

    changed from html to ejs

--- myapp/server.js
+++ myapp/server.js
@@ -9,7 +9,7 @@ const path = require('path')
 //shows what is happening on the server and post it on the terminal(Logger)
 app.use(morgan('short'))
 
-app.set('views', path.join(__dirname, 'public')); //changed from 'views' to 'public'
+app.set('public', path.join(__dirname, 'public')); //changed from 'views' to 'public'
 app.set('view engine', 'ejs'); //changed from 'jade' to 'ejs'
 
 //body Parser is a middleware
@@ -19,28 +19,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 //index
-app.get('/index', function(req, res) {
+app.get('/', function(req, res) {
   res.render('index');
 });
 
-// catch 404 and forward to error handler
-app.all(function(req, res, next) {
-  next(createError(404));
-});
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-
-app.get("/" , function(req , res , next){
+//profil
+app.get("/profil" , function(req , res , next){
   res.render('profil');
 })
 //app.get("/", (req, res) => {
