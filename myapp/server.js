9fbf3889efd22348ac7e0709a5c47aac9ea140de
SysRQ95 <33806385+SysRQ95@users.noreply.github.com>
2020-01-13 14:07:37 +0100

    hi

--- myapp/server.js
+++ myapp/server.js
@@ -112,7 +112,7 @@ app.get('/ressourcen/co2schaetzung', function(req, res) {
 
 app.
 
-app.post('/', function(req , res) {
+app.post('/', (req , res) => {
   const db = req.getConnection()
   console.log("Trying to log in..")
   console.log("First name: " + req.body.VornameInput);
