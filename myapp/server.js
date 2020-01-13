596dfe7fa00fe099017ddd10a236734fc4ad002d
SysRQ95 <33806385+SysRQ95@users.noreply.github.com>
2020-01-13 12:29:31 +0100

    ll

--- myapp/server.js
+++ myapp/server.js
@@ -118,7 +118,7 @@ app.post('141.45.92.87:3003/profil', (req , res) => {
   const Nachname = req.body.NachnameInput;
   
 
-  const queryString = "INSERT INTO DoriDB.nutzer_tb (nutzer_name, nutzer_vorname) SET (default,?,?,?,?,?,?,?,?)";
+  const queryString = "INSERT INTO DoriDB.nutzer_tb (nutzer_name, nutzer_vorname) SET (NULL,?,?,?,?,?,?,?,?)";
   getConnection().query(queryString, [Vorname , Nachname] , (err, result , fields) => {if (err) {
     Console.log ("Failed to update user data..." + err);
     res.sendStatus(500);
