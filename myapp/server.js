24425cb92d17e3140737f340a560067fc3146bb9
Malte <s0563114@htw-berlin.de>
2020-01-14 17:25:15 +0100

    testing table view

--- myapp/server.js
+++ myapp/server.js
@@ -29,8 +29,8 @@ app.get('/', function (req, res) {
   });
 //Maßnahmenkatalog
 app.get('/massnahmen-katalog', function (req, res) {
-  var queryString = "SELECT res_kategorie_tb.res_kategorie_id, res_kategorie_tb.res_kategorie_name, massnahmen_tb.massnahmen_id, massnahmen_tb.massnahmen_name, massnahmen_tb.massnahmen_beschreibung FROM res_kategorie_tb, massnahmen_tb GROUP BY massnahmen_tb.massnahmen_id ORDER BY res_kategorie_tb.res_kategorie_id";
-  // var queryString = "SELECT res_kategorie_tb.res_kategorie_id, res_kategorie_tb.res_kategorie_name, massnahmen_tb.massnahmen_name, massnahmen_tb.massnahmen_beschreibung FROM massnahmen_tb RIGHT JOIN ressourcen_tb ON massnahmen_tb.massnahmen_res_kategorie = ressourcen_tb.ressourcen_kategorie RIGHT JOIN res_kategorie_tb ON ressourcen_tb.ressourcen_kategorie = res_kategorie_tb.res_kategorie_id ORDER BY ressourcen_tb.ressourcen_kategorie";
+  // var queryString = "SELECT res_kategorie_tb.res_kategorie_id, res_kategorie_tb.res_kategorie_name, massnahmen_tb.massnahmen_id, massnahmen_tb.massnahmen_name, massnahmen_tb.massnahmen_beschreibung FROM res_kategorie_tb, massnahmen_tb GROUP BY massnahmen_tb.massnahmen_id ORDER BY res_kategorie_tb.res_kategorie_id";
+  var queryString = "SELECT res_kategorie_tb.res_kategorie_id, res_kategorie_tb.res_kategorie_name, massnahmen_tb.massnahmen_name, massnahmen_tb.massnahmen_beschreibung FROM massnahmen_tb INNER JOIN ressourcen_tb ON massnahmen_tb.massnahmen_res_kategorie = ressourcen_tb.ressourcen_kategorie INNER JOIN res_kategorie_tb ON ressourcen_tb.ressourcen_kategorie = res_kategorie_tb.res_kategorie_id ORDER BY ressourcen_tb.ressourcen_kategorie";
 
   getConnection().query(queryString, function (err, result) {
     if (err) {
