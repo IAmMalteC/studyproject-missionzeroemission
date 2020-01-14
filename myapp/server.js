30cc4f632b1b418894abe3aec555dddf38717cd4
Malte <s0563114@htw-berlin.de>
2020-01-14 17:12:36 +0100

    testing table view

--- myapp/server.js
+++ myapp/server.js
@@ -29,7 +29,7 @@ app.get('/', function (req, res) {
   });
 //Maßnahmenkatalog
 app.get('/massnahmen-katalog', function (req, res) {
-  var queryString = "SELECT res_kategorie_tb.res_kategorie_id, res_kategorie_tb.res_kategorie_name, massnahmen_tb.massnahmen_name, massnahmen_tb.massnahmen_beschreibung FROM massnahmen_tb CROSS JOIN ressourcen_tb ON massnahmen_tb.massnahmen_res_kategorie = ressourcen_tb.ressourcen_kategorie INNER JOIN res_kategorie_tb ON ressourcen_tb.ressourcen_kategorie = res_kategorie_tb.res_kategorie_id ORDER BY ressourcen_tb.ressourcen_kategorie";
+  var queryString = "SELECT res_kategorie_tb.res_kategorie_id, res_kategorie_tb.res_kategorie_name, massnahmen_tb.massnahmen_name, massnahmen_tb.massnahmen_beschreibung FROM massnahmen_tb LEFT JOIN ressourcen_tb ON massnahmen_tb.massnahmen_res_kategorie = ressourcen_tb.ressourcen_kategorie LEFT JOIN res_kategorie_tb ON ressourcen_tb.ressourcen_kategorie = res_kategorie_tb.res_kategorie_id ORDER BY ressourcen_tb.ressourcen_kategorie";
 
   getConnection().query(queryString, function (err, result) {
     if (err) {
