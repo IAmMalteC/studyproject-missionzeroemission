ba6942a9804ffb93fd27111089fd79812315242b
Malte <s0563114@htw-berlin.de>
2020-01-14 14:26:26 +0100

    fixing dropdown menu

--- myapp/public/javascripts/changeMonthOrYearView.js
+++ myapp/public/javascripts/changeMonthOrYearView.js
@@ -1,12 +1,16 @@
-document.getElementById("Ablesung").addEventListener("change", showMonthOrYear);  
+document.getElementById("Ablesung").addEventListener("change", showMonthOrYear);
 
 function showMonthOrYear() {
-  var e  = document.getElementById("Ablesung");
+  var e = document.getElementById("Ablesung");
   var zeitraum = e.options[e.selectedIndex].value;
-  if (zeitraum == "jaehrlich") {
-    document.getElementsByClassName("ZeitraumMonat").style.display = "none";
-  }
-  else {
-    document.getElementsByClassName("ZeitraumMonat").style.display = "inline";
+
+  var x = document.getElementsByClassName("ZeitraumMonat");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    if (zeitraum == "jaehrlich") {
+      x[i].style.display = "none";
+    } else {
+      x[i].style.display = "inline";
+    }
   }
 }
