18bb59504491dcb67d1f417fd9a2670410fe18fa
Malte <s0563114@htw-berlin.de>
2020-01-15 12:52:33 +0100

    working on hamburger menu

--- myapp/public/javascripts/menu.js
+++ myapp/public/javascripts/menu.js
@@ -4,12 +4,3 @@ $(document).ready(function () {
     $('#' + element).addClass('active');
 });
 
-//Makes the menu to an hamburger menu
-function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-}
-
-document.querySelector('.Navbar__Link-toggle')
-        .addEventListener('click', classToggle);
