96edcb9ed72364bd58337fba4ab2949cfdbb763d
Malte <s0563114@htw-berlin.de>
2020-01-15 12:21:44 +0100

    working on hamburger menu

--- myapp/public/javascripts/menu.js
+++ myapp/public/javascripts/menu.js
@@ -1,4 +1,30 @@
-$(document).ready(function(){
+//This searches for the acitve Link
+$(document).ready(function () {
     var element = $('meta[name="active-menu"]').attr('content');
     $('#' + element).addClass('active');
 });
+
+//Makes the menu to an hamburger menu
+function makeHamburgerMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// function showMonthOrYear() {
+//     var e = document.getElementById("Ablesung");
+//     var zeitraum = e.options[e.selectedIndex].value;
+
+//     var x = document.getElementsByClassName("ZeitraumMonat");
+//     var i;
+//     for (i = 0; i < x.length; i++) {
+//       if (zeitraum == "jaehrlich") {
+//         x[i].style.display = "none";
+//       } else {
+//         x[i].style.display = "inline";
+//       }
+//     }
+//   }
