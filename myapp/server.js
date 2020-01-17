a68e8643e3812c1f51a886d6de1727c18b7aa447
Malte <s0563114@htw-berlin.de>
2020-01-17 23:40:30 +0100

    refactoring first running graph

--- myapp/server.js
+++ myapp/server.js
@@ -57,6 +57,8 @@ var index_path = ['/', '/index'];
 app.get(index_path,
   findRevenueTotal,
   renderIndexPage);
+
+  
 //Maßnahmenkatalog
 app.get('/massnahmen-katalog', function (req, res) {
   var queryString = "SELECT res_kategorie_tb.res_kategorie_id, res_kategorie_tb.res_kategorie_name, massnahmen_tb.massnahmen_name, massnahmen_tb.massnahmen_beschreibung FROM massnahmen_tb INNER JOIN res_kategorie_tb ON massnahmen_tb.massnahmen_res_kategorie = res_kategorie_tb.res_kategorie_id ORDER BY res_kategorie_tb.res_kategorie_id";
