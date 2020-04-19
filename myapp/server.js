deb62c603c22242669ecd5761d006104fb21950f
SysRQ95 <33806385+SysRQ95@users.noreply.github.com>
2020-04-19 21:38:24 +0200

    cc

--- myapp/server.js
+++ myapp/server.js
@@ -236,6 +236,7 @@ app.get('/ressourcen/strom', function (req, res) {
     res.render('./ressourcen/strom', { page: 'Strom', menuId: 'strom' });
   }else{
     res.redirect('/index')
+    res.send(500, 'ShowAlert')
   }
   });
 app.post('/strom', function (req, res) {
